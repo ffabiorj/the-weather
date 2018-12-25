bdcee541e0a226cfa280e61bd31b04a97781e152
Fabio Oliveira <fabio20rj@gmail.com>
2018-12-25 10:40:42 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The weather
+
+- This app was made to learn how to use the basics of lib request.
---
+++ core/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+import requests
+from .models import City
+from .forms import CityForm
+from django.contrib import messages
+
+def home(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=36a83bb3097fd2cad04a91b0bfe6b45c'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+        messages.success(request, 'A cidade foi adicionada com sucesso')
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        temp_weather = {
+            'id': city.id,
+            'city': r['name'],
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(temp_weather)
+    context = {
+        'temp_weather': weather_data, 'form': form,
+        }
+    return render(request, 'index.html', context)
+
+def delete_weather(request, id):
+    weather = City.objects.get(id=id)
+    weather.delete()
+    messages.success(request, 'A cidade foi deletada com sucesso')
+    return redirect(home)
